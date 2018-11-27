ca6cf63f4c7fb2247267206674f52347932958ec
marcusp619 <marcusp619@gmail.com>
2018-11-26 17:10:26 -0700

    Server serves static index.html file

--- public/js/server.js
+++ public/js/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker API';
