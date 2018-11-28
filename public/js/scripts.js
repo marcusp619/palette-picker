7e4bc25cf6f322342bb9ff6a81cb1a38e5dfc622
marcusp619 <marcusp619@gmail.com>
2018-11-28 13:31:25 -0700

    Add POST route for projects

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,21 @@
-console.log('scripts file hooked up')
+const colorGenerator = () => {
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let hexNum = ['#'];
+  let totalArr = []
+  for(let i = 0; i < 6; i++) {
+    let randomItem = arr[Math.floor(Math.random() * arr.length)];
+    hexNum.push(randomItem)
+  }
+  
+  return hexNum.join('');
+}
+
+const findNewColor = () => {
+  const activeColors = document.querySelectorAll('active')
+  let newColors = [];
+
+  for(let i = 1; i < activeColors.length; i++) {
+    newColors.push(hexGenerator())
+  }
+  return newColors
+}
--- public/js/server.js
+++ public/js/server.js
@@ -7,23 +7,28 @@ app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker API';
-app.locals.colors = [{id: 1, color: '#000000'}, {id: 2, color: '#F5F5F5'}];
+app.locals.palettes = [{id: 1, color: '#000000'}, {id: 2, color: '#F5F5F5'}];
+app.locals.projects = [{id: 1}];
 
-app.get('/', (request, response) => {
-  response.send('Testing Palette Picker API');
-});
-
-app.get('/api/v1/colors', (request, response) => {
+app.get('/api/v1/projects/palettes', (request, response) => {
   response.send('Testing Colors GET End Point');
 });
 
-app.post('/api/v1/colors', (request, response) => {
+app.get('/api/v1/palettes/:id', (request, response) => {
+  const { id } = request.params;
+  const palette = app.locals.palettes.find(palette => palette.id === id);
+
+  return response.status(200).json(palette);
+});
+
+
+app.post('/api/v1/projects', (request, response) => {
   const id = Date.now();
-  const {color} = request.body;
+  const { project } = request.body;
+  app.locals.projects.push(project)
 
-  app.locals.colors.push(color);
 
-  return response.status(201).json({ id, color});
+  return response.status(201).json({ id, project});
 });
 
 app.listen(app.get('port'), () => {
