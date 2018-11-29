8e4ee2f7d1878519af77b97b036cc9fa2bb5bad8
marcusp619 <marcusp619@gmail.com>
2018-11-28 19:26:26 -0700

    Add Generate random colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,37 +1,46 @@
-const colorGenerator = () => {
-  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-  let hexNum = ['#'];
-  let totalArr = []
-  for(let i = 0; i < 6; i++) {
-    let randomItem = arr[Math.floor(Math.random() * arr.length)];
-    hexNum.push(randomItem)
-  }
-  
-  return hexNum.join('');
-}
-
-const findNewColor = () => {
-  const activeColors = document.querySelectorAll('active')
-  let newColors = [];
-
-  for(let i = 1; i < activeColors.length; i++) {
-    newColors.push(hexGenerator())
-  }
-  return newColors
-}
-
 class ColorPicker {
   constructor() {
-    this.projects = [],
+    this.projects = [];
   }
 
-  getColors = () => {
-  
-  }
+  getColors() {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexNum = ['#'];
+    let totalArr = [];
+    for (let i = 0; i < 6; i++) {
+      let randomItem = arr[Math.floor(Math.random() * arr.length)];
+      hexNum.push(randomItem);
+    }
 
-  addProject = project => {
-  
-  }
+    return hexNum.join('');
+  };
 
-  
+  findNewColor() {
+    const activeColors = document.querySelectorAll('.palette-squares');
+    let newColors = [];
+
+    for (let i = 0; i < activeColors.length; i++) {
+      newColors.push(this.getColors());
+    }
+    console.log(newColors);
+
+    activeColors.forEach((color, i) => {
+      color.setAttribute('style', `background-color:${newColors[i]}`)
+      console.log(color.parentElement.childNodes[0])
+    })
+    return newColors;
+  };
+
+  addProject(project){};
+
+  addPalette(palette){};
+
+  deletePalette(palette){};
+
+  toggleFreeze(event){
+    //toggle class active or disable
+  };
 }
+
+const newUser = new ColorPicker();
+newUser.findNewColor();
